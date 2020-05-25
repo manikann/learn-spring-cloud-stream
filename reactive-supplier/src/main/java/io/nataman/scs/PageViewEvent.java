0f275eb1e6bfb76e28403ddfffc2b26a1ece13a1
Manikandan Natarajan <manikann@gmail.com>
2020-05-25 10:50:07 +0800

    Add bean validation to reactive consumer

--- reactive-supplier/src/main/java/io/nataman/scs/PageViewEvent.java
+++ reactive-supplier/src/main/java/io/nataman/scs/PageViewEvent.java
@@ -1,7 +1,8 @@
 package io.nataman.scs;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 import lombok.With;
 
@@ -10,7 +11,12 @@ import lombok.With;
 @Builder
 public class PageViewEvent {
 
-  @NonNull String userid;
-  @NonNull String page;
+  @NotBlank(message = "userid is mandatory")
+  String userid;
+
+  @NotBlank(message = "page is mandatory")
+  String page;
+
+  @Min(value = 0, message = "duration cannot be negative")
   int duration;
 }
--- reactive-supplier/src/main/java/io/nataman/scs/ReactiveSupplierConsumerApplication.java
+++ reactive-supplier/src/main/java/io/nataman/scs/ReactiveSupplierConsumerApplication.java
@@ -5,8 +5,12 @@ import static org.springframework.kafka.support.converter.KafkaMessageHeaders.CO
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
 
 import java.time.Duration;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import javax.validation.Validator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +21,10 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @Log4j2
+@RequiredArgsConstructor
 public class ReactiveSupplierConsumerApplication {
+
+  private final Validator validator;
 
   public static void main(String[] args) {
     SpringApplication.run(ReactiveSupplierConsumerApplication.class, args);
@@ -48,6 +55,34 @@ public class ReactiveSupplierConsumerApplication {
   @Bean
   public Consumer<Flux<Message<PageViewEvent>>> eventSink(
       Consumer<Message<PageViewEvent>> consumer) {
-    return flux -> flux.log("eventSink").subscribe(consumer);
+    return flux -> flux.log("eventSink").filter(this::validateMessage).subscribe(consumer);
+  }
+
+  private boolean validateMessage(Message<PageViewEvent> m) {
+    log.debug("validateMessage: {}", m);
+    var errorMessage =
+        validator.validate(m.getPayload()).stream()
+            .map(
+                v -> {
+                  log.debug("getConstraintDescriptor: {}", v.getConstraintDescriptor());
+                  log.debug("getExecutableParameters: {}", v.getExecutableParameters());
+                  log.debug("getExecutableReturnValue: {}", v.getExecutableReturnValue());
+                  log.debug("getInvalidValue: {}", v.getInvalidValue());
+                  log.debug("getPropertyPath: {}", v.getPropertyPath());
+                  log.debug("getMessage: {}", v.getMessage());
+                  return String.format(
+                      "{ path: '%s', invalidValue: '%s', message: '%s'}",
+                      v.getPropertyPath(), v.getInvalidValue(), v.getMessage());
+                })
+            .collect(Collectors.joining(";"));
+    Optional.of(errorMessage)
+        .filter(e -> !e.isBlank())
+        .ifPresent(
+            s -> {
+              var t = new RuntimeException(s);
+              log.error("validateMessage failed: [{}]", s, t);
+              throw t;
+            });
+    return true;
   }
 }
